e01e0a1206d3664252f1fecb1afe473029549694
compig <172124359+zxcv-asdf-qwer@users.noreply.github.com>
2022-06-09 15:14:08 +0900

    refator : upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.6.2"
+    id("org.springframework.boot") version "2.6.8"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.6.0"
-    kotlin("plugin.spring") version "1.6.0"
-    kotlin("plugin.jpa") version "1.6.0"
-    kotlin("plugin.allopen") version "1.6.0"
-    kotlin("kapt") version "1.5.30"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.jpa") version "1.6.21"
+    kotlin("kapt") version "1.5.32"
+//    kotlin("plugin.allopen") version "1.6.21"
 }
 
 noArg {
@@ -35,33 +35,31 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-rest")
-    implementation("org.springframework.boot:spring-boot-starter-security:2.6.3")
-    implementation("org.springframework.boot:spring-boot-starter-validation:2.6.3")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 
-    implementation("org.springframework.security.oauth.boot:spring-security-oauth2-autoconfigure:2.6.3")
     implementation("org.springframework.social:spring-social-core:1.1.6.RELEASE")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.10")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-common:1.6.10")
 
-    implementation("io.github.microutils:kotlin-logging:1.12.5") // Logging
+    implementation("io.github.microutils:kotlin-logging:2.1.21") // Logging
 
-    runtimeOnly("com.h2database:h2:2.0.206")
-    implementation("mysql:mysql-connector-java:8.0.28")
+    runtimeOnly("com.h2database:h2:2.1.212")
+    implementation("mysql:mysql-connector-java:8.0.29")
 
-    compileOnly("org.projectlombok:lombok:1.18.22")
+    compileOnly("org.projectlombok:lombok:1.18.24")
     implementation("org.projectlombok:lombok-mapstruct-binding:0.2.0")
 
     implementation("org.apache.commons:commons-lang3:3.12.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
-    implementation("io.jsonwebtoken:jjwt-api:0.11.2")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
-    implementation("org.modelmapper:modelmapper:3.0.0")
+    implementation("org.modelmapper:modelmapper:3.1.0")
     implementation("org.mapstruct:mapstruct:1.4.2.Final")
     kapt("org.mapstruct:mapstruct-processor:1.4.2.Final")
     kaptTest("org.mapstruct:mapstruct-processor:1.4.2.Final")
@@ -69,6 +67,7 @@ dependencies {
     implementation("com.google.code.gson:gson:2.9.0")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
 
 }
 
